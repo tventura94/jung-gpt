c84638c4503c226953dcb5ed5aa995e51b9ccce5
tventura94 <tventura94@gmail.com>
2023-07-08 15:31:26 -0400

    attempt at build

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://https://incredible-torte-364240.netlify.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
