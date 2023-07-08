0030de7a16f0caea7637c451333f33735515b4cf
tventura94 <tventura94@gmail.com>
2023-07-08 14:39:02 -0400

    cors update

--- server/index.js
+++ server/index.js
@@ -15,14 +15,14 @@ const openai = new OpenAIApi(configuration);
 const app = express();
 
 app.use(bodyParser.json());
-
-app.use(
-  cors({
-    origin: "https://incredible-torte-364240.netlify.app/",
-    credentials: true,
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "https://incredible-torte-364240.netlify.app/",
+//     credentials: true,
+//     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   })
+// );
 
 const port = process.env.PORT || 3080;
 
