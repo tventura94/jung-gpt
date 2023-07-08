22fa62141b566260214188c28606eb630ba18dd3
tventura94 <tventura94@gmail.com>
2023-07-08 13:03:36 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://jung-gpt.onrender.com/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
