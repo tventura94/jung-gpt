be1873365647486f602ede603898ed4d5041ac23
tventura94 <tventura94@gmail.com>
2023-07-11 21:23:58 -0400

    2nd attempt to add DBT bot

--- server/index2.js
+++ server/index2.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 const configuration = new Configuration({
   organization: "org-d3pQZk3os1Tsy721vAbe4j3M",
-  apiKey: process.env.OPEN_AI_API_KEY,
+  apiKey: process.env.API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
