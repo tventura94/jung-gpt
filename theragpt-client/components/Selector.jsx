c9d456b2e76050abab99fc3a4f1ceb21316ad50a
tventura94 <tventura94@gmail.com>
2023-07-11 23:59:50 -0400

    added animations and framer-motion library

--- theragpt-client/components/Selector.jsx
+++ theragpt-client/components/Selector.jsx
@@ -3,6 +3,7 @@ import { Box, Typography, Button } from "@mui/material";
 import Dashboard from "./Dashboard";
 import MenuPopup from "./MenuPopup";
 import MenuPopupState from "./MenuPopup";
+import { motion } from "framer-motion";
 
 export default function Selector({ setUser, setAuthState, user }) {
   return (
@@ -31,40 +32,83 @@ export default function Selector({ setUser, setAuthState, user }) {
             width: "97%",
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "40vh",
-              width: "40vw",
-              backgroundColor: "#ddd",
-              borderRadius: "1em",
-              margin: "0em",
+          <motion.div
+            className="box"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.3,
+              delay: 0.1,
+              ease: [0, 0.71, 0.2, 1.01],
             }}
           >
-            <Button onClick={() => setAuthState("dashboard")}>
-              <Typography variant="h4">Jung GPT</Typography>
-            </Button>
-          </Box>
-
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "40vh",
-              width: "40vw",
-              backgroundColor: "#ddd",
-              borderRadius: "1em",
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "40vh",
+                width: "40vw",
+                backgroundColor: "#ddd",
+                borderRadius: "1em",
+                margin: "0em",
+                transition: "transform 0.15s ease-in-out", // add this line
+                ":hover": {
+                  transform: "scale(1.05)", // add this line
+                },
+              }}
+            >
+              <Button onClick={() => setAuthState("dashboard")}>
+                <Typography variant="h4">Jung GPT</Typography>
+              </Button>
+              <Typography m={5}>
+                Our very first Emotional Reflection Feedback tool. This tool is
+                trained in psychology, psychiatry, philosophy, and medicine. It
+                is used to help users gain clarity into how they might be
+                feeling, and discover ways in which they might be able to help
+                solve or deal with their day to day problems.
+              </Typography>
+            </Box>
+          </motion.div>
+          <motion.div
+            className="box"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.3,
+              delay: 0.2,
+              ease: [0, 0.71, 0.2, 1.01],
             }}
           >
-            <Button onClick={() => setAuthState("dbt")}>
-              <Typography variant="h4">Jung DBT</Typography>
-            </Button>
-          </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "40vh",
+                width: "40vw",
+                backgroundColor: "#ddd",
+                borderRadius: "1em",
+                margin: "0em",
+                transition: "transform 0.15s ease-in-out", // add this line
+                ":hover": {
+                  transform: "scale(1.05)", // add this line
+                },
+              }}
+            >
+              <Button onClick={() => setAuthState("dbt")}>
+                <Typography variant="h4">Jung DBT</Typography>
+              </Button>
+              <Typography m={5}>
+                Our custom DBT centered tool. This tool is Dialectical
+                Behavioral Therapy. It helps users reframe thoughts, learn DBT
+                skills, and even gives them worksheets to work on based on their
+                own personal issues.
+              </Typography>
+            </Box>
+          </motion.div>
         </Box>
       </div>
     </div>
