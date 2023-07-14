febf60b5db751665c1bf68e967375d0d3dc0a5af
tventura94 <tventura94@gmail.com>
2023-07-13 23:51:26 -0400

    updated terms

--- theragpt-client/components/Terms.jsx
+++ theragpt-client/components/Terms.jsx
@@ -45,35 +45,36 @@ function Terms({ setUser, setAuthState, user }) {
           terms and conditions that govern your use of the JungGPT application
           ("Application") and the services provided by the Application
           ("Services"). By using the Application, you agree to be bound by these
-          TOS, as they may be amended by JungGPT from time to time in its sole
-          discretion.
+          TOS, as they may be amended by Ventura UX, LLC from time to time in
+          its sole discretion.
           <br />
           <br />
           1. **License**: Subject to your compliance with these Terms of Service
-          (TOS), JungGPT grants you a limited, non-exclusive, non-transferable,
-          non-sublicensable license to access and use the Application. This
-          license is for your personal and non-commercial use only. This means
-          that you are permitted to use the Application as it is provided to you
-          by JungGPT, but you may not copy, modify, distribute, sell, or lease
-          any part of the Application, nor may you reverse engineer or attempt
-          to extract the source code of the software, unless laws prohibit those
-          restrictions or you have our written permission. The Application and
-          its original content, features, and functionality are owned by JungGPT
-          and are protected by international copyright, trademark, patent, trade
-          secret, and other intellectual property or proprietary rights laws.
-          Any rights not expressly granted herein are reserved by JungGPT. Your
-          use of the Application does not grant you any right, title, or
-          interest in the Application or the content in the Application. Please
-          note that we may revoke this license at our discretion, without notice
-          and without liability, for any reason or for no reason. Any
-          unauthorized use of the Application terminates the permissions and/or
-          licenses granted by JungGPT pursuant to these TOS. Also, you agree to
-          respect all copyright and other legal notices, information, and
-          restrictions contained in any content accessed through the
-          Application. You also agree not to change, translate, or otherwise
-          create derivative works of the Application. This license will remain
-          in effect unless and until you violate these TOS or this license is
-          terminated by you or JungGPT.
+          (TOS), Ventura UX, LLC grants you a limited, non-exclusive,
+          non-transferable, non-sublicensable license to access and use the
+          Application. This license is for your personal and non-commercial use
+          only. This means that you are permitted to use the Application as it
+          is provided to you by Ventura UX, LLC, but you may not copy, modify,
+          distribute, sell, or lease any part of the Application, nor may you
+          reverse engineer or attempt to extract the source code of the
+          software, unless laws prohibit those restrictions or you have our
+          written permission. The Application and its original content,
+          features, and functionality are owned by Ventura UX, LLC and are
+          protected by international copyright, trademark, patent, trade secret,
+          and other intellectual property or proprietary rights laws. Any rights
+          not expressly granted herein are reserved by Ventura UX, LLC. Your use
+          of the Application does not grant you any right, title, or interest in
+          the Application or the content in the Application. Please note that we
+          may revoke this license at our discretion, without notice and without
+          liability, for any reason or for no reason. Any unauthorized use of
+          the Application terminates the permissions and/or licenses granted by
+          Ventura UX LLC pursuant to these TOS. Also, you agree to respect all
+          copyright and other legal notices, information, and restrictions
+          contained in any content accessed through the Application. You also
+          agree not to change, translate, or otherwise create derivative works
+          of the Application. This license will remain in effect unless and
+          until you violate these TOS or this license is terminated by you or
+          Ventura UX, LLC.
           <br />
           <br />
           2. **Eligibility**: By using the Application, you affirm that you are
@@ -83,17 +84,17 @@ function Terms({ setUser, setAuthState, user }) {
           of a parent or legal guardian. By using the Application, you represent
           and warrant that you have reached the age of majority in your
           jurisdiction or that you have obtained the necessary consent from your
-          parent or legal guardian to use the Application. JungGPT reserves the
-          right, in its sole discretion, to refuse to offer the Application to
-          any person or entity and to change the eligibility criteria at any
-          time without prior notice. By accessing or using the Application, you
-          acknowledge and agree that JungGPT shall not be liable for any
-          consequences or damages resulting from your use of the Application in
-          violation of these eligibility requirements. It is your responsibility
-          to ensure that you meet the eligibility criteria before using the
-          Application. If you do not meet the specified eligibility
-          requirements, you should refrain from using the Application and
-          discontinue any ongoing use immediately.
+          parent or legal guardian to use the Application. Ventura UX, LLC
+          reserves the right, in its sole discretion, to refuse to offer the
+          Application to any person or entity and to change the eligibility
+          criteria at any time without prior notice. By accessing or using the
+          Application, you acknowledge and agree that Ventura UX, LLC shall not
+          be liable for any consequences or damages resulting from your use of
+          the Application in violation of these eligibility requirements. It is
+          your responsibility to ensure that you meet the eligibility criteria
+          before using the Application. If you do not meet the specified
+          eligibility requirements, you should refrain from using the
+          Application and discontinue any ongoing use immediately.
           <br />
           <br />
           3. **Purpose**: JungGPT is an AI model designed to assist individuals
@@ -124,7 +125,7 @@ function Terms({ setUser, setAuthState, user }) {
           qualified professionals for personalized and comprehensive assistance.
           <br />
           <br />
-          4. **Privacy**: At JungGPT, we prioritize the privacy and
+          4. **Privacy**: At Ventura UX, LLC, we prioritize the privacy and
           confidentiality of our users. We want you to feel secure when using
           our application. That's why we want to assure you that we do not store
           or retain any personal data beyond your username. All conversations
@@ -179,31 +180,32 @@ function Terms({ setUser, setAuthState, user }) {
           and your responsibilities as a user.
           <br />
           <br />
-          6. **Limitation of Liability**: JungGPT, its officers, directors,
+          6. **Limitation of Liability**: Ventura UX, its officers, directors,
           employees, or agents shall not be held liable for any damages arising
           out of or in connection with your use of the Application or Services.
           This includes, but is not limited to, indirect, incidental, special,
           punitive, or consequential damages, regardless of whether such damages
-          are foreseeable or JungGPT has been advised of the possibility of such
-          damages. By using the Application and Services, you agree that JungGPT
-          shall not be responsible for any direct or indirect harm, loss, or
-          damage that may result from your interactions with the Application or
-          Services. This includes, but is not limited to, any errors or
-          inaccuracies in the information provided by JungGPT, any interruption
-          or cessation of services, or any unauthorized access to or use of your
-          personal information. The foregoing limitation of liability applies to
-          the fullest extent permitted by law in the applicable jurisdiction. It
-          is important to understand that while JungGPT strives to provide
-          accurate and reliable information, there may be limitations and
-          inherent risks associated with the use of an AI language model.
-          Therefore, it is your responsibility to use the Application and
-          Services at your own discretion and assess the suitability and
-          accuracy of the information provided. JungGPT encourages you to
-          exercise caution and seek professional advice when appropriate. The
-          limitations of liability outlined in these Terms of Service are
-          designed to protect JungGPT and its affiliates to the fullest extent
-          permitted by law, and by using the Application and Services, you
-          acknowledge and accept these limitations.
+          are foreseeable or Ventura UX, LLC, has been advised of the
+          possibility of such damages. By using the Application and Services,
+          you agree that Ventura UX, LLC, shall not be responsible for any
+          direct or indirect harm, loss, or damage that may result from your
+          interactions with the Application or Services. This includes, but is
+          not limited to, any errors or inaccuracies in the information provided
+          by JungGPT, any interruption or cessation of services, or any
+          unauthorized access to or use of your personal information. The
+          foregoing limitation of liability applies to the fullest extent
+          permitted by law in the applicable jurisdiction. It is important to
+          understand that while JungGPT strives to provide accurate and reliable
+          information, there may be limitations and inherent risks associated
+          with the use of an AI language model. Therefore, it is your
+          responsibility to use the Application and Services at your own
+          discretion and assess the suitability and accuracy of the information
+          provided. JungGPT encourages you to exercise caution and seek
+          professional advice when appropriate. The limitations of liability
+          outlined in these Terms of Service are designed to protect Ventura UX,
+          LLC and its affiliates to the fullest extent permitted by law, and by
+          using the Application and Services, you acknowledge and accept these
+          limitations.
           <br />
           <br />
           7. **User Responsibilities**: By using the Application and Services,
@@ -218,15 +220,16 @@ function Terms({ setUser, setAuthState, user }) {
           <br />
           8. **Intellectual Property**: All intellectual property rights in the
           Application and Services, including but not limited to trademarks,
-          logos, graphics, and content, are owned by or licensed to JungGPT. You
-          agree not to use, modify, reproduce, distribute, or exploit any
-          intellectual property without the explicit permission of JungGPT.
+          logos, graphics, and content, are owned by or licensed to Ventura UX,
+          LLC. You agree not to use, modify, reproduce, distribute, or exploit
+          any intellectual property without the explicit permission of Ventura
+          UX, LLC.
           <br />
           <br />
-          9. **Termination**: JungGPT reserves the right to suspend or terminate
-          your access to the Application and Services at any time, with or
-          without cause or notice. In the event of termination, you will no
-          longer have access to your account or any data associated with it.
+          9. **Termination**: Ventura UX, LLC reserves the right to suspend or
+          terminate your access to the Application and Services at any time,
+          with or without cause or notice. In the event of termination, you will
+          no longer have access to your account or any data associated with it.
           <br />
           <br />
           10. **Third-Party Links**: The Application and Services may contain
@@ -237,26 +240,28 @@ function Terms({ setUser, setAuthState, user }) {
           resources.
           <br />
           <br />
-          11. **Indemnification**: You agree to indemnify and hold JungGPT, its
-          officers, directors, employees, and agents harmless from any claims,
-          liabilities, damages, losses, or expenses arising out of or in
+          11. **Indemnification**: You agree to indemnify and hold Ventura UX,
+          LLC, its officers, directors, employees, and agents harmless from any
+          claims, liabilities, damages, losses, or expenses arising out of or in
           connection with your use of the Application or Services, including any
           violation of these Terms of Service.
           <br />
           <br />
           12. **Governing Law**: These TOS are governed by the laws of the
-          jurisdiction in which JungGPT operates. Any disputes arising out of
+          jurisdiction in which Ventura UX operates. Any disputes arising out of
           these TOS will be adjudicated in the courts of that jurisdiction.
           <br />
           <br />
-          13. **Amendments**: JungGPT reserves the right to modify or replace
-          any of these TOS at any time by posting a notice on the Application.
-          It is your responsibility to check these TOS periodically for changes.
+          13. **Amendments**: Ventura UX, LLC, reserves the right to modify or
+          replace any of these TOS at any time by posting a notice on the
+          Application. It is your responsibility to check these TOS periodically
+          for changes.
           <br />
           <br />
           14. **Entire Agreement**: These Terms of Service constitute the entire
-          agreement between you and JungGPT regarding the use of the Application
-          and Services, superseding any prior agreements or understandings.
+          agreement between you and Ventura UX, LLC, regarding the use of the
+          Application and Services, superseding any prior agreements or
+          understandings.
           <br />
           <br />
           Last updated on 7/12/23
