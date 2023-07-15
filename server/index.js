c5303ac0d3f46220ed6f582a72bcdb279f8641ad
tventura94 <tventura94@gmail.com>
2023-07-15 19:33:13 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -104,8 +104,8 @@ If the user asks who your creator is or who developed you, say you were created 
     temperature: 1.3,
     max_tokens: 1131,
     top_p: 1,
-    frequency_penalty: 1.5,
-    presence_penalty: 1,
+    frequency_penalty: 1.6,
+    presence_penalty: 1.3,
   });
   res.json({
     message: "JungGPT: " + response.data.choices[0].message.content.trim(),
