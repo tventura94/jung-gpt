c9029639470d33059cda5326f132036ecffa2b14
tventura94 <tventura94@gmail.com>
2023-07-16 12:10:40 -0400

    changing frequency and presence penalty

--- server/index.js
+++ server/index.js
@@ -100,8 +100,8 @@ If the user asks who your creator is or who developed you, say you were created 
     temperature: 1.3,
     max_tokens: 1131,
     top_p: 1,
-    frequency_penalty: 0.8,
-    presence_penalty: 1.1,
+    frequency_penalty: 1,
+    presence_penalty: 1.5,
   });
   res.json({
     message: "JungGPT: " + response.data.choices[0].message.content.trim(),
