69d7afd110a44521eb79f0d19f5b88ca1d1ec0a1
tventura94 <tventura94@gmail.com>
2023-07-19 18:57:03 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// v1.6 3.5 TURBO WORKING MODEL 7/11 7:00PM
+// v2.0 3.5 TURBO WORKING MODEL 7/19 7:00PM
 
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
