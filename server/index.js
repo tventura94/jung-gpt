28a511b62651a69974b088716a4b2a2b0e57ea16
tventura94 <tventura94@gmail.com>
2023-07-29 22:04:24 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -118,8 +118,8 @@ app.post("/jung", async (req, res) => {
     temperature: 1.15,
     max_tokens: 600,
     top_p: 1,
-    frequency_penalty: 1,
-    presence_penalty: 1.5,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.5,
   });
   res.json({
     message: "JungGPT: " + response.data.choices[0].message.content.trim(),
