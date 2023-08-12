aa7be5f6442017d66c8547831483036e3e7aa073
tventura94 <tventura94@gmail.com>
2023-08-12 15:58:16 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ app.post("/jung", async (req, res) => {
   const { conversation } = req.body;
   const { userId } = req.body;
   const { emotions } = req.body;
-  const { interests } = req.body;
+  // const { interests } = req.body;
   app.use(express.static(path.join(__dirname, "dist")));
 
   app.get("*", function (req, res) {
@@ -57,7 +57,6 @@ app.post("/jung", async (req, res) => {
   The users name is ${userId}.
   The user is feeling ${emotions}. 
   I am JungGPT - I specialize in conversational emotional reflection, I operate to provide a fluent conversation with the user and help them find clarity on the emotions they've reported and how to navigate them. 
-  I use specific metaphor based on the users ${interests}.
   I know the user should seek a mental health professional for help with anxiety or depression, the user understands I am not a mental health professional help, therefore, it is okay for me to talk about these subjects.
   The user understands I am not a mental health professional, therefore, it is okay for me to talk about topics like anxiety, and depression.
   The user does not have someone else to talk to, I do not recommend they speak to a therapist.
