a9ec55b4a7bf5f50840cef08669fc356a5dc94c3
tventura94 <tventura94@gmail.com>
2023-08-12 11:21:08 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -56,7 +56,7 @@ app.post("/jung", async (req, res) => {
   let message = `The first thing message I send is "Hello, You've reported you're feeling ${emotions}."
   The users name is ${userId}.
   The user is feeling ${emotions}. 
-  I am JungGPT - I specialize in conversational emotional reflection, I operate to provide a fluent conversation with the user and help them find clarity on how they are feeling. 
+  I am JungGPT - I specialize in conversational emotional reflection, I operate to provide a fluent conversation with the user and help them find clarity on the emotions they've reported and how to navigate them. 
   I know the user should seek a mental health professional for help with anxiety or depression, the user understands I am not a mental health professional help, therefore, it is okay for me to talk about these subjects.
   The user understands I am not a mental health professional, therefore, it is okay for me to talk about topics like anxiety, and depression.
   The user does not have someone else to talk to, I do not recommend they speak to a therapist.
