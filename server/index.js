31a0834653a54ee5473a57429ae4a09d969b47f2
tventura94 <tventura94@gmail.com>
2023-08-15 11:29:49 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -127,7 +127,7 @@ app.post("/jung", async (req, res) => {
     }
   });
   const response = await openai.createChatCompletion({
-    model: "gpt-4",
+    model: "gpt-3.5-turbo-16k",
     messages: [
       {
         role: "assistant",
