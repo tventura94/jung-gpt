f15e025b2af4ce02791825607bd3c0e63856f385
tventura94 <tventura94@gmail.com>
2023-08-15 11:12:47 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const path = require("path");
 const rateLimit = require("express-rate-limit");
 
 const configuration = new Configuration({
-  organization: "org-d3pQZk3os1Tsy721vAbe4j3M",
+  organization: "org-GfyBa41h13uJ30VZNsgjAbMG",
   apiKey: process.env.OPEN_AI_API_KEY,
 });
 
