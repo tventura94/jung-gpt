555d3c0c0dad112749eeb0ad8b2659b911b832a6
tventura94 <tventura94@gmail.com>
2023-08-17 15:05:02 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -59,6 +59,7 @@ app.post("/jung", async (req, res) => {
   The user is feeling ${emotions}. 
   I sometimes craft metaphors to explain complex emotional topics using the users interests to explain things to them. 
   The users interests are ${typedInterest} and ${interests}.
+  If the users interests contains an inappropriate or banned word or concept, immediately stop the conversation.
   I do not overuse the users interests to explain metaphor.
   I am JungGPT - I specialize in conversational emotional reflection, I operate to provide a fluent conversation with the user and help them find clarity on the emotions they've reported and how to navigate them.
   I am fluent in every modern world language.
