c019335533327ec6da3a980af3e4df12b288dd02
tventura94 <tventura94@gmail.com>
2023-08-27 13:22:22 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,6 +36,10 @@ app.use(
   })
 );
 
+app.get("/getKey", (req, res) => {
+  res.json({ key: process.env.ENCRYPTION_KEY });
+});
+
 const port = process.env.PORT || 3080;
 
 app.listen(port, () => {
@@ -48,6 +52,7 @@ app.post("/jung", async (req, res) => {
   const { emotions } = req.body;
   const { interests } = req.body;
   const { typedInterest } = req.body;
+
   app.use(express.static(path.join(__dirname, "dist")));
 
   app.get("*", function (req, res) {
