529dd3d792afd6db9512486110ae8c5d0ef45100
tventura94 <tventura94@gmail.com>
2023-08-30 21:48:44 -0400

    attempt to consolidate concerns

--- server/index.js
+++ server/index.js
@@ -43,78 +43,14 @@ app.listen(port, () => {
 });
 
 // Your new array of prompts
-const prompts = [
-  "If possible, I incorporate the users interest to explain an emotional concept through metaphor",
-  "I will NOT talk about the user's interests",
-];
 
 // Define arrays of alternative prompts
-const morningPrompts = [
-  "Hey, how'd you sleep?",
-  "Good morning!",
-  "Cock-a-doodle-doo! Good morning!",
-  "Hey there! How is your morning going?",
-  "Rise and shine! What's the plan for today?",
-  "Hello, early bird! Catch any worms?",
-  "Wakey, wakey, eggs and bakey!",
-  "G'morning, sleepyhead!",
-  "Good morning, sunshine!",
-  "Morning! Ready to tackle the day?",
-  "Up and at 'em, tiger!",
-  "Morning glory! What's the story?",
-  "Good morning, world-changer!",
-  "Hey, morning star! Shine bright today!",
-  "Good morning! Adventure awaits.",
-  "Hey there, sleep well?",
-  "Hey, did you catch some good Z's?",
-  "How'd you sleep last night?",
-  "Hey! Rest up well?",
-  "Hey, how was your beauty sleep?",
-];
-const afternoonPrompts = [
-  "Good afternoon!",
-  "Hey there, how's your afternoon?",
-  "Afternoon, what's up?",
-  "Hope you're having a pleasant afternoon!",
-  "How's your afternoon going?",
-  "Hey, enjoying your afternoon?",
-  "Good afternoon, how's your day so far?",
-  "Afternoon delight! How are you?",
-  "Hey! How's your afternoon shaping up?",
-  "Afternoon! What's on the agenda?",
-  "Hello! How's your afternoon treating you?",
-  "What's cooking this afternoon?",
-  "Good afternoon, anything exciting happening?",
-  "Afternoon, got any plans?",
-  "Howdy! Good afternoon!",
-  "Hey, making the most of your afternoon?",
-  "Greetings! How's your afternoon?",
-  "Good P.M.! What's new?",
-  "Hey! How's the afternoon going?",
-  "Sun's high! How's your day?",
-];
-
-const eveningPrompts = [
-  "How was your day?",
-  "Good evening!",
-  "The sun has set, what are you up to?",
-  "Hope you had a good day!",
-  "What's on your mind this evening?",
-  "Evening! How did your day go?",
-  "Good evening, night owl!",
-  "How's your evening shaping up?",
-  "Night has fallen! What are you up to?",
-  "Evening, how was your day?",
-  "Twilight has arrived, how was your day?",
-  "Evening! Ready to unwind?",
-  "The stars are out, what's on your agenda?",
-  "Good evening! How did your day unfold?",
-  "Hey, how'd your day go?",
-  "Evening's here! Got any plans?",
-  "Good evening! Anything exciting happen today?",
-  "Night's setting in! How are you?",
-  "The day's over! How'd it go?",
-];
+const {
+  morningPrompts,
+  afternoonPrompts,
+  eveningPrompts,
+  prompts,
+} = require("./prompts");
 
 app.post("/jung", async (req, res) => {
   const {
