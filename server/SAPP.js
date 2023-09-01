5f5c64b2c2e1e5c9fea3405de92ec5ef34deb65a
tventura94 <tventura94@gmail.com>
2023-09-01 12:22:34 -0400

    Update SAPP.js

--- server/SAPP.js
+++ server/SAPP.js
@@ -241,8 +241,8 @@ async function SAPP(lastUserMessage) {
       const [result] = await client.analyzeSentiment({ document: document });
       const sentimentScore = result.documentSentiment.score;
 
-      console.log(`Sentence: ${relevantSentence}`);
-      console.log(`Sentiment Score: ${sentimentScore}`);
+      // console.log(`Sentence: ${relevantSentence}`);
+      // console.log(`Sentiment Score: ${sentimentScore}`);
 
       switch (keywordStem) {
         case "exist":
