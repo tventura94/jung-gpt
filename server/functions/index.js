2e2ef7110441d0d61b9087308f97367d0243ea93
tventura94 <tventura94@gmail.com>
2023-09-02 16:05:48 -0400

    Update index.js

--- server/functions/index.js
+++ server/functions/index.js
@@ -6,48 +6,50 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-
-const functions = require("firebase-functions");
+// Import necessary modules and setup
 require("dotenv").config();
-
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-exports.createCheckoutSession = functions.https.onCall(
-  async (data, context) => {
-    try {
-      // Hardcoded price ID for metered billing
-      const meteredPriceId = "price_1NlwVJGx3uwFHp11F4HC6UDu";
-
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            price: meteredPriceId,
-            // No quantity specified for metered billing
-          },
-          {
-            price_data: {
-              currency: "usd",
-              product_data: {
-                name: "One-time fee",
-              },
-              unit_amount: 700, // $7.00 in cents
+// Define your createCheckoutSession function
+exports.createCheckoutSession = onRequest(async (request, response) => {
+  try {
+    // Hardcoded price ID for metered billing
+    const meteredPriceId = "price_1NlwVJGx3uwFHp11F4HC6UDu";
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: meteredPriceId,
+          // No quantity specified for metered billing
+        },
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "One-time fee",
             },
-            quantity: 1,
+            unit_amount: 700, // $7.00 in cents
           },
-        ],
-        mode: "payment",
-        success_url: "https://jung-gpt.com",
-        cancel_url: "https://jung-gpt.com",
-      });
-
-      return { sessionId: session.id };
-    } catch (error) {
-      console.error("Stripe session creation failed: ", error);
-      return { error: "Failed to create Stripe session." };
-    }
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: "https://jung-gpt.com",
+      cancel_url: "https://jung-gpt.com",
+    });
+    return response.json({ sessionId: session.id });
+  } catch (error) {
+    console.error("Stripe session creation failed: ", error);
+    return response
+      .status(500)
+      .json({ error: "Failed to create Stripe session." });
   }
-);
+});
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
