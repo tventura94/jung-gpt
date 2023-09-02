55b1d43e7818805aadecc194ddc36debf2c4d4c5
tventura94 <tventura94@gmail.com>
2023-09-02 01:48:27 -0400

    trying to update variable to total_tokens

--- server/index.js
+++ server/index.js
@@ -177,10 +177,12 @@ ${thought}
     frequency_penalty: 0.3,
     presence_penalty: 0.5,
   });
+
   res.json({
     message: "JungGPT: " + response.data.choices[0].message.content.trim(),
-    usage: response.data.usage,
+    usage: response.data.usage.total_tokens,
   });
+  console.log(usage);
 });
 
 app.post("/dbt", async (req, res) => {
