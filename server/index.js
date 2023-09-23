2755297a2b77a4a35516afa7fa1f4494e36b2b0b
tventura94 <tventura94@gmail.com>
2023-09-22 23:14:19 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // LIVE https://jung-gpt.com    DEV http://localhost:5173
+    origin: "https://jung-gpt.com", // LIVE https://jung-gpt.com    DEV http://localhost:5173
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
