8bff7f478fae4b55c2fe2dc04880eea2e1a9a7fe
tventura94 <tventura94@gmail.com>
2023-09-27 13:11:41 -0400

    adding prerender

--- server/index.js
+++ server/index.js
@@ -36,6 +36,9 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.use(
+  require("prerender-node").set("prerenderToken", "JPVrKsKW7401ZxKu1y1c")
+);
 
 const port = process.env.PORT || 3080;
 
