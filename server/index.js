5183f95082f506a7b737f7e2011a80d20dd5f138
tventura94 <tventura94@gmail.com>
2023-09-27 14:17:05 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use(
 );
 
 app.use(
-  require("prerender-node").set("prerenderToken", "JPVrKsKW7401ZxKu1y1c")
+  require("prerender-node").set("prerenderToken", "uBSRQK8cLuh683Dfz5Fh")
 );
 
 const port = process.env.PORT || 3080;
