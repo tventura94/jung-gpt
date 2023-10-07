8474fd6a22b89de284e462380ab55c531d67630e
tventura94 <tventura94@gmail.com>
2023-10-06 21:35:51 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -332,10 +332,10 @@ app.post("/whisper", upload.single("audio"), async (req, res) => {
     const transcribedText = await transcribeAudio(audioPath);
     console.log("Transcribed text:", transcribedText);
 
-    const summaryMessage = `Please summarize in one or two paragraphs the following mental-health related conversation for a mental health professional.
+    const summaryMessage = ` Respond in JSON Format. Please summarize in one or two paragraphs the following mental-health related conversation for a mental health professional.
     Capture the key issues, emotional tone, and any notable concerns that a mental health professional should be aware of.
-    Rate the severity of each concern on a scale of 1-5, where 1 is least severe and 5 is most severe.
-    Additionally, suggest potential action items based on the conversation. Deliver response in JSON format.`;
+    Rate the severity of each concern on a scale of 1-5, where 1 is least severe and 5 is most severe. 
+    Additionally, suggest potential action items based on the conversation. Return the key issues and severity rating in a key value pair JSON object. Return the action items in a JSON object`;
 
     const summaryResponse = await openai.chat.completions.create({
       model: "gpt-3.5-turbo-16k",
