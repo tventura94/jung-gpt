5c19966b06087f1ad3225399ea039ee83c87aad0
tventura94 <tventura94@gmail.com>
2023-10-07 13:56:19 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -548,7 +548,7 @@ app.post("/whisper", upload.single("audio"), async (req, res) => {
           content: transcribedText,
         },
       ],
-      temperature: 0.5,
+      temperature: 0,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
