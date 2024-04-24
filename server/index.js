97e1345e07157259cf43f84129ca0433039c6e9f
tventura94 <tventura94@gmail.com>
2024-04-24 08:07:22 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -231,25 +231,20 @@ app.post("/jung", async (req, res) => {
     presence_penalty: 0.5,
     stream: true,
   });
-  let accumulatedContent = "";
+
   try {
     for await (const chunk of response) {
       if (chunk.choices[0]?.delta?.content) {
-        accumulatedContent += chunk.choices[0].delta.content;
-        if (accumulatedContent.length >= 100) {
-          res.write(
-            `data: ${JSON.stringify({ message: accumulatedContent })}\n\n`
-          );
-          accumulatedContent = "";
-        }
+        res.write(
+          `data: ${JSON.stringify({
+            message: chunk.choices[0].delta.content,
+          })}\n\n`
+        );
       }
-    }
-    if (accumulatedContent.length > 0) {
-      res.write(`data: ${JSON.stringify({ message: accumulatedContent })}\n\n`);
     }
   } catch (error) {
     console.error("Error processing stream:", error);
-    res.end();
+    res.end;
   }
   res.on("close", () => {
     res.end();
